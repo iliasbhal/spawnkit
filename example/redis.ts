400599c93ec0b029eb163f13fcb94f190bcccf13
ilias bhallil <ilias.bhal@gmail.com>
2024-03-26 18:38:38 +0700

    nit: add example of client usage

--- example/redis.ts
+++ example/redis.ts
@@ -20,3 +20,11 @@ const client = Spawnkit.Client.from({
   adapters: spawnORM,
   instances: [],
 });
+
+const main = async () => {
+  const invitation = client.actor("AAA", 123213);
+  await invitation.send({ type: "ELEVATE" });
+  await invitation.get();
+  invitation.on("event", (event) => {});
+  invitation.on("data", (data) => {});
+};
