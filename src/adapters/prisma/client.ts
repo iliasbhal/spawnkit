ddf8c4c80581fda4780f3fc4d2ac9219c32dd75d
ilias bhallil <ilias.bhal@gmail.com>
2024-03-31 19:48:19 +0700

    chore: remove remove redis from prisma

--- src/adapters/prisma/client.ts
+++ src/adapters/prisma/client.ts
@@ -1,19 +1,7 @@
 require("dotenv-mono").load();
-import { Redis, RedisOptions } from "ioredis";
-
 import { PrismaClient } from "./generated/client";
 
 export * as Zod from "./generated/zod";
 
 export * from "./generated/client";
 export const ORM = new PrismaClient();
-
-export const redisConfig: RedisOptions = {
-  port: parseInt(process.env.REDIS_PORT!),
-  host: process.env.REDIS_HOST,
-  maxRetriesPerRequest: 0,
-  enableAutoPipelining: true,
-  showFriendlyErrorStack: true,
-};
-
-export const redis = new Redis(redisConfig);
