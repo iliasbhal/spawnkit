aef267488e68f3df4b4ccc25305e85404ced2726
ilias bhallil <ilias.bhal@gmail.com>
2024-04-11 09:07:20 +0700

    update: data is cached to avoid unnecessary network calls

--- src/models/Data.test.ts
+++ src/models/Data.test.ts
@@ -4,5 +4,7 @@ describe("Data", () => {
   it.todo("should be able to set and get data");
   it.todo("should return null if data is not set yet");
   it.todo("should cache data per key");
+  it.todo("should cleanup the cache when a key is not used");
   it.todo("should only call set once per tick max");
+  it.todo("should use fencing token when setting data and abort if didnt work");
 });
--- src/models/Data.ts
+++ src/models/Data.ts
@@ -5,6 +5,58 @@ export class Data<DataShape extends Record<string, any>> {
   adapters: Pick<Adapters, "data" | "messages">;
   instanceId: InstanceId;
 
+  cache = new Cache();
+
+  constructor(config: {
+    adapters: Pick<Adapters, "data" | "messages">;
+    instanceId: InstanceId;
+  }) {
+    this.adapters = config.adapters;
+    this.instanceId = config.instanceId;
+  }
+
+  static getChannelForInstanceKeyUpdates(instanceId: string, key: string) {
+    return `${instanceId}:key:${key}:data-update`;
+  }
+
+  async get<K extends keyof DataShape>(key: K): Promise<DataShape[K] | null> {
+    if (this.cache.has(key)) {
+      return this.cache.get(key);
+    }
+
+    const data = await this.adapters.data.get(this.instanceId, key.toString());
+    this.cache.set(key, data);
+    return data;
+  }
+
+  debounceByKey = new Map<keyof DataShape, AsyncDebounceHandler>();
+  async set<K extends keyof DataShape>(key: K, value: DataShape[K]) {
+    this.cache.set(key, value);
+
+    if (!this.debounceByKey.has(key)) {
+      this.debounceByKey.set(key, new AsyncDebounceHandler());
+    }
+
+    const debouncer = this.debounceByKey.get(key)!;
+    await debouncer.onlyLastOnePerTick(async () => {
+      await this.adapters.data.set(this.instanceId, key.toString(), value);
+
+      const channel = Data.getChannelForInstanceKeyUpdates(
+        this.instanceId,
+        key.toString(),
+      );
+      await this.adapters.messages.publish(channel, value);
+      // cleanup to ensure we don't end up with a big object
+      // in the case where the instance is using a lot of keys
+      this.debounceByKey.delete(key);
+    });
+  }
+}
+
+export class RemoteData<DataShape extends Record<string, any>> {
+  adapters: Pick<Adapters, "data" | "messages">;
+  instanceId: InstanceId;
+
   constructor(config: {
     adapters: Pick<Adapters, "data" | "messages">;
     instanceId: InstanceId;
@@ -17,33 +69,35 @@ export class Data<DataShape extends Record<string, any>> {
     return await this.adapters.data.get(this.instanceId, key.toString());
   }
 
-  debounceByKey = new Map<keyof DataShape, AsyncDebounceHandler>();
-  async set<K extends keyof DataShape>(key: K, value: DataShape[K]) {
-    if (!this.debounceByKey.has(key)) {
-      this.debounceByKey.set(key, new AsyncDebounceHandler());
-    }
-
-    const debouncer = this.debounceByKey.get(key)!;
-
-    await debouncer.onlyLastOnePerTick(async () => {
-      await this.adapters.data.set(this.instanceId, key.toString(), value);
-
-      const channel = this.getChannelForInstanceKeyUpdates(key.toString());
-      await this.adapters.messages.publish(channel, value);
-      // cleanup to ensure we don't end up with a big object
-      // in the case where the instance is using a lot of keys
-      this.debounceByKey.delete(key);
-    });
-  }
-
-  getChannelForInstanceKeyUpdates(key: string) {
-    return `${this.instanceId}:key:${key}:data-update`;
-  }
-
   on<K extends keyof DataShape>(key: K, callback: (next: DataShape[K]) => any) {
-    const channel = this.getChannelForInstanceKeyUpdates(key.toString());
+    const channel = Data.getChannelForInstanceKeyUpdates(
+      this.instanceId,
+      key.toString(),
+    );
     return this.adapters.messages.subscribe<DataShape[K]>(channel, (event) => {
       callback(event.data);
     });
+  }
+}
+
+class Cache extends Map {
+  get(key: any) {
+    this.scheduleCleanup(key);
+    return super.get(key);
+  }
+
+  set(key: any, value: any) {
+    super.set(key, value);
+    this.scheduleCleanup(key);
+    return this;
+  }
+
+  timeouts: Record<any, any> = {};
+  private scheduleCleanup(key: any) {
+    const timeoutId = setTimeout(() => {
+      this.delete(key);
+    }, 10_000);
+
+    this.timeouts[key] = timeoutId;
   }
 }
