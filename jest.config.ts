c630eac39e9565b959a48e9a0dfde00e9c44c2dc
ilias bhallil <ilias.bhal@gmail.com>
2024-10-18 08:50:00 +0700

    fix: test couldnt run because of module

--- jest.config.ts
+++ jest.config.ts
@@ -3,11 +3,18 @@ import { JestConfigWithTsJest } from "ts-jest";
 const esModules = ["superjson", "nanoid"].join("|");
 
 const jestConfig: JestConfigWithTsJest = {
-	preset: "ts-jest/presets/js-with-ts",
+	preset: "ts-jest",
+
 	testEnvironment: "node",
 	transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
 	moduleNameMapper: {
 		"@/(.*)$": "<rootDir>/src/$1",
+	},
+	transform: {
+		"^.+\\.jsx?$": "babel-jest",
+		"^.+\\.tsx?$": ["ts-jest", {
+			isolatedModules: true,
+		}],
 	},
 };
 
