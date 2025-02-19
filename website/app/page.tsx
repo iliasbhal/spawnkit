a2fbddbb05b64b5ac5990a7971354366d71dfc76
ilias bhallil <ilias.bhal@gmail.com>
2025-02-20 00:24:34 +0700

    update landing page

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -22,6 +22,7 @@ import {
 	PopoverTrigger,
 } from "@/components/ui/popover";
 import { Logo } from "@/components/logo";
+import { motion } from "motion/react";
 
 const features = [
 	{
@@ -81,26 +82,64 @@ export default function HomePage() {
 		<main className="h-min mx-auto overflow-x-hidden">
 				<div className="md:w-10/12 my-20 mx-auto font-geist relative  rounded-none -pr-2">
 			<div className="w-full md:mx-0">
-			<div className="relative col-span-3 md:border-[1.2px] h-full py-40">
-					<div className="w-full h-full p-16 pt-10 md:px-10">
-						<div className="absolute top-0 left-0 w-full h-full z-0">
-							<HeroBackground />
-						</div>
+			<div className="relative col-span-3 md:border-[1.2px] h-full">
+					<div className="w-full h-full">
 						<div className="flex flex-col items-center justify-end w-full h-full gap-10 z-1 relative">
-							<div className="flex flex-col items-center justify-center w-full h-full gap-10 z-1 relative">
+							<div className="absolute top-0 left-0 w-full h-full z-0">
+								<HeroBackground />
+							</div>
+							<div className="flex flex-col items-center justify-center w-full  gap-10 z-1 relative py-40">
 								<div 
-									className="relativew-20 h-6"
+									className="relative w-20 h-6"
 									style={{
 										width: 449 * 1.3,
 										height: 114 * 1.3,
 									}}
 								>
-									<Logo />
+									<motion.div 
+										className="w-full h-full absolute top-0 left-0"
+										initial={{ 
+											opacity: 0, 
+											scale: 0.9,
+											translateY: -30 
+										}}
+										animate={{ 
+											opacity: 1,
+											scale: 1,
+											translateY: 0 
+										}} 
+										transition={{ 
+											type: "spring",
+											bounce: 0.2,
+										}}
+									>
+										<Logo hideKitPart={true} />
+									</motion.div>
+									<motion.div 
+										className="w-full h-full absolute top-0 left-0"
+										initial={{ 
+											opacity: 0,
+											scale: 0.9,
+											translateY: -30 
+										}} 
+										animate={{ 
+											opacity: 1, 
+											scale: 1,
+											translateY: 0
+										 }} 
+										transition={{ 
+											delay: 0.1,
+											type: "spring",
+											bounce: 0.2,
+										}}
+									>
+										<Logo hideSpawnPart={true}/>
+									</motion.div>
 								</div>
 								<div className="flex flex-col items-center justify-center w-full h-full gap-3 z-1 relative">
 								<div className="flex items-center gap-2">
 								<Link href="/docs" className="cursor-pointer hover:bg-primary/30">
-									<Button variant="outline" className="p-6 font-normal text-lg">Documentation</Button>
+									<Button variant="outline" className="p-6 font-normal text-lg bg-primary/60 hover:bg-primary/80">Documentation</Button>
 								</Link>
 
 								<Popover hideTimeout={2000}>
@@ -112,7 +151,9 @@ export default function HomePage() {
 													navigator.clipboard.writeText("npm install spawnkit");
 												}}
 											>
-												<span className="font-mono font-normal">~ npm install spawnkit</span>
+												<span className="font-mono font-normal opacity-70">
+													<span className="opacity-70">{'>'}</span> <span className="opacity-70">npm install</span> spawnkit
+												</span>
 												<Copy className="w-4 h-4" />
 											</Button>
 										</PopoverTrigger>
@@ -124,80 +165,65 @@ export default function HomePage() {
 
 								{/* <GithubStat stars={stars} /> */}
 								</div>
+								<div className="h-3" />
 								<p className="mx-auto text-xl font-thin tracking-tighter text-center">
 									Roll your own auth with confidence in minutes!<br />
 									No vendor lock-in, no proprietary code.
 								</p>
 							</div>
 							</div>
-							{/* <div className="flex z-20 justify-center items-start">
-								<TechStackDisplay
-									skills={[
-										"nextJs",
-										"nuxt",
-										"svelteKit",
-										"astro",
-										// "solidStart",
-										// "react",
-										// "hono",
-										"expo",
-										"tanstack",
-									]}
-								/>
-							</div> */}
 						</div>
-					</div>
-				</div>
-
-				<div className="h-20" />
 				
-				<div className="grid grid-cols-1 relative md:grid-rows-2 md:grid-cols-3 border-b-[1.2px] border-r-[1.2px]">
-					<div className="hidden md:grid top-1/2 left-0 -translate-y-1/2 w-full grid-cols-3 z-10 pointer-events-none select-none absolute">
-						<Plus className="w-8 h-8 text-neutral-300 translate-x-[16.5px] translate-y-[.5px] ml-auto dark:text-neutral-600" />
-						<Plus className="w-8 h-8 text-neutral-300 ml-auto translate-x-[16.5px] translate-y-[.5px] dark:text-neutral-600" />
-					</div>
-					{features.map((feature, index) => (
-						<div
-							key={feature.id}
-							className={cn(
-								"justify-center border-l-[1.2px] md:min-h-[240px] border-t-[1.2px] transform-gpu flex flex-col p-10",
-								// index >= 3 && "border-b-[1.2px]",
-								// index % 3 === 0 && "border-r-[1.2px]"
-							)}
-						>
-							<div className="flex items-center gap-2 my-1">
-								<feature.icon className="w-4 h-4" />
-								<p className="text-gray-600 dark:text-gray-400 font-mono">
-									{feature.label}
-								</p>
+						<div className="grid grid-cols-1 relative md:grid-rows-2 md:grid-cols-3 border-b-[1.2px] border-r-[1.2px]">
+							<div className="hidden md:grid top-1/2 left-0 -translate-y-1/2 w-full grid-cols-3 z-10 pointer-events-none select-none absolute">
+								<Plus className="w-8 h-8 text-neutral-300 translate-x-[16.5px] translate-y-[.5px] ml-auto dark:text-neutral-600" />
+								<Plus className="w-8 h-8 text-neutral-300 ml-auto translate-x-[16.5px] translate-y-[.5px] dark:text-neutral-600" />
 							</div>
-							<div className="mt-2">
-								<div className="max-w-full">
-									<div className="flex gap-3 ">
-										<p
-											className="max-w-lg text-xl font-normal tracking-tighter md:text-2xl"
-											dangerouslySetInnerHTML={{
-												__html: feature.title,
-											}}
-										/>
+							{features.map((feature, index) => {
+								const isStartOfRow = index % 3 === 0;
+								const isEndOfRow = index % 3 === 2;
+								return (
+									<div
+									key={feature.id}
+									className={cn(
+										"justify-center md:min-h-[240px] border-t-[1.2px] transform-gpu flex flex-col p-10",
+										{
+											"border-l-[1.2px]": !isStartOfRow,
+											"border-r-[1.2px]": !isEndOfRow,
+										}
+									)}
+								>
+									<div className="flex items-center gap-2 my-1">
+										<feature.icon className="w-4 h-4" />
+										<p className="text-gray-600 dark:text-gray-400 font-mono">
+											{feature.label}
+										</p>
+									</div>
+									<div className="mt-2">
+										<div className="max-w-full">
+											<div className="flex gap-3 ">
+												<p
+													className="max-w-lg text-xl font-normal tracking-tighter md:text-2xl"
+													dangerouslySetInnerHTML={{
+														__html: feature.title,
+													}}
+												/>
+											</div>
+										</div>
+										<p className="mt-2 text-sm text-left text-muted-foreground">
+											{feature.description}
+											<a className="ml-2 underline" href="/docs" target="_blank">
+												Learn more
+											</a>
+										</p>
 									</div>
 								</div>
-								<p className="mt-2 text-sm text-left text-muted-foreground">
-									{feature.description}
-									<a className="ml-2 underline" href="/docs" target="_blank">
-										Learn more
-									</a>
-								</p>
-							</div>
+								)
+							})}
 						</div>
-					))}
+					</div>
 				</div>
 
-				<div className="h-20" />
-
-				<div className="w-full hidden md:block md:border-[1.2px]">
-					<Testimonial />
-				</div>
 			</div>
 		</div>
 
