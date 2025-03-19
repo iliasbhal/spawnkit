3ec6fb30a4f3e3505ca8466cb917238c23c811b9
ilias bhallil <ilias.bhal@gmail.com>
2025-03-19 11:32:48 +0700

    minor: remove unused functions

--- src/core/InstanceUtils.ts
+++ src/core/InstanceUtils.ts
@@ -11,7 +11,6 @@ export const DATA_UTILS_NAMESPACE = '__INTERNAL__UTILS__';
 
 /** These methods can be called from client */
 export class InstanceUtils<Inst extends Instance> extends BaseRemoteEntity<Inst> {
-  private __types = {} as Inst['__types'];
   private utilsData: Data<InternalInstanceData>;
 
   constructor(data: Data<any>) {
@@ -36,17 +35,5 @@ export class InstanceUtils<Inst extends Instance> extends BaseRemoteEntity<Inst>
   async setLastInitialized(timestamp: number) {
     return await this.utilsData.set('last_initialized', timestamp);
   }
-
-  // lock = {
-  //   lock: async () => {
-  //     return this.utilsData.set('is_locked', true);
-  //   },
-  //   unlock: async () => {
-  //     return this.utilsData.set('is_locked', false);
-  //   },
-  //   check: async () => {
-  //     return this.utilsData.get('is_locked');
-  //   },
-  // }
 }
 
