07e202dc7e13cd89acc6aebbc21cf96da8ba5b9b
ilias bhallil <ilias.bhal@gmail.com>
2025-03-22 01:39:50 +0700

    test: add pubsub test

--- src/tests/pubsub.test.ts
+++ src/tests/pubsub.test.ts
@@ -1,85 +1,94 @@
-import * as Spawnkit from "../src";
-
-interface OrderBookData {
-  orderBook: string[];
-  count: number;
-}
-
-interface OrderBookEvent {
-  buyOrders: any[];
-  orders: any[];
-  alphachannel: string;
-}
-
-interface OrderBookContext {
-  userID: string;
-}
-
-interface Stock {
-  tick: string;
-}
-
-
-
-interface Order extends Pick<Stock, "tick"> {
-  qty: number;
-}
-
-export class OrderBook extends Spawnkit.Instance<OrderBookContext, OrderBookData, OrderBookEvent> {
-  initialize(): void {
-
-  }
-
-  on<C extends keyof OrderBookEvent>(channel: C, message: OrderBookEvent[C]) {
-    if (channel === 'alphachannel') {
-
-    }
-
-    if (channel === "buyOrders") {
-      const mdg = message;
-      this.emit("orders", mdg as any);
-      return;
-    }
-    // console.log("ON INSTANCE", this.id, channel, message);
-  }
-
-  async buy(order: Order) {
-    this.context.userID
-    // this.logger.log("-----BUYYYYY------");
-    // console.log("___BUY___", order);
-    // this.data = this.data || ({} as any);
-    // this.data!.count = this.data?.count || 0;
-    // this.data!.count++;
-
-    // const interval = setInterval(() => {
-    this.emit("orders", [order.tick, order.qty]);
-    // })
-    // setTimeout(() => {
-    //   clearInterval(interval);
-    // }, 400);
-
-    return {
-      success: true,
-      status: "pending...",
-      order,
-    };
-  }
-
-  async multiple(...stocks: Stock[]) {
-    return stocks.length;
-  }
-
-  async sell(stock: Stock): Promise<true> {
-    return true;
-  }
-}
-
-
-
-
+import * as Spawnkit from "../../src";
+import { waitFor } from "poll-until-promise";
+import { baseRedisAdapters } from "../../src/adapters/redis/base";
+import { wait } from "@/utils/wait";
 
 describe("PubSub", () => {
-  it.todo("can emit and listen to instance channels");
-  it.todo("when subscrbing to channel, it should not replay past events");
+
+  interface Channels {
+    enterChat: { userId: string; }
+  }
+
+  interface Context {
+    userID: string;
+  }
+
+  class PubSubExample extends Spawnkit.Instance<Context, Channels> {
+    on<C extends keyof Channels>(channel: C, message: Channels[C]) {
+      // console.log("ON INSTANCE", this.id, channel, message);
+    }
+
+    enterChat() {
+      this.emit("enterChat", {
+        userId: this.context.userID,
+      });
+    }
+
+  }
+
+
+  const client = Spawnkit.Client.from({
+    adapters: baseRedisAdapters,
+    instances: {
+      PubSubExample,
+    },
+  });
+
+  client.start();
+
+  it("can emit and listen to instance channels", async () => {
+    const instance = client.spawn("PubSubExample", "test", {
+      userID: "test-user-id",
+    });
+
+    const callback = jest.fn();
+    instance.on("enterChat", callback);
+
+    instance.enterChat();
+
+    await waitFor(() => {
+      expect(callback).toHaveBeenCalled();
+      expect(callback).toHaveBeenCalledTimes(1);
+      expect(callback).toHaveBeenCalledWith(
+        expect.objectContaining({ userId: "test-user-id" })
+      );
+    }, {
+      interval: 10,
+    });
+
+    instance.dispose();
+
+  });
+
+  it("when subscrbing to channel, it should not replay past events", async () => {
+    const instance = client.spawn("PubSubExample", "test-2", {
+      userID: "test-user-id",
+    });
+
+    const callback = jest.fn();
+
+    await instance.enterChat();
+
+    await wait(300);
+
+    instance.on("enterChat", callback);
+
+    await waitFor(() => {
+      expect(callback).not.toHaveBeenCalled();
+    }, {
+      interval: 10,
+    });
+
+    instance.enterChat();
+
+    await waitFor(() => {
+      expect(callback).toHaveBeenCalled();
+      expect(callback).toHaveBeenCalledTimes(1);
+    }, {
+      interval: 10,
+    });
+
+    instance.dispose();
+  });
 });
 
