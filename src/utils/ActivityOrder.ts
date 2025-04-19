feaceae979c9d06a075e36a5e9b4d933bf01e691
ilias bhallil <ilias.bhal@gmail.com>
2025-04-20 01:09:56 +0700

    update: extract function into own function

--- src/utils/ActivityOrder.ts
+++ src/utils/ActivityOrder.ts
@@ -5,24 +5,24 @@ export class ActivityOrder {
 
   waitForOrder(order: number) {
     const eventPromise = this.ensureOrderFilled(order);
-
-    Promise.resolve().then(async () => {
-      for (let i = order; i < this.promiseByOrder.length; i++) {
-        const eventPromise = this.promiseByOrder[i];
-        if (!eventPromise) return;
-
-        const isAlreadyFulfilled = eventPromise?.fulfilled;
-        if (isAlreadyFulfilled) return;
-
-        const prevEventPromise = this.promiseByOrder[i - 1];
-        const isInOrder = i === 0 || prevEventPromise?.fulfilled;
-        if (!isInOrder) return;
-
-        eventPromise.resolve(i);
-      }
-    });
-
+    Promise.resolve().then(() => this.fulfillOrder(order));
     return eventPromise.await;
+  }
+
+  async fulfillOrder(order: number) {
+    for (let i = order; i < this.promiseByOrder.length; i++) {
+      const eventPromise = this.promiseByOrder[i];
+      if (!eventPromise) return;
+
+      const isAlreadyFulfilled = eventPromise?.fulfilled;
+      if (isAlreadyFulfilled) return;
+
+      const prevEventPromise = this.promiseByOrder[i - 1];
+      const isInOrder = i === 0 || prevEventPromise?.fulfilled;
+      if (!isInOrder) return;
+
+      eventPromise.resolve(i);
+    }
   }
 
   ensureOrderFilled(order: number) {
