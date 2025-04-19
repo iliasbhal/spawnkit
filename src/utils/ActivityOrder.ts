aae55025cc36796509629ca50cbccd5ec9f480c4
ilias bhallil <ilias.bhal@gmail.com>
2025-04-20 01:44:30 +0700

    update: do not use Promise.resolve

--- src/utils/ActivityOrder.ts
+++ src/utils/ActivityOrder.ts
@@ -1,31 +1,31 @@
 import { ControlledPromise } from './ControlledPromise'
 
 export class ActivityOrder {
-  promiseByOrder: ControlledPromise<number>[] = [];
+  private promiseByOrder: ControlledPromise<number>[] = [];
 
   waitForOrder(order: number) {
     const eventPromise = this.ensureOrderFilled(order);
-    Promise.resolve().then(() => this.fulfillOrder(order));
-    return eventPromise.await;
+
+    return new Promise((resolve) => {
+      eventPromise.await.then(resolve)
+      this.fulfillCorrectlyOrdered(order)
+    })
   }
 
-  async fulfillOrder(order: number) {
-    for (let i = order; i < this.promiseByOrder.length; i++) {
-      const eventPromise = this.promiseByOrder[i];
-      if (!eventPromise) return;
+  private async fulfillCorrectlyOrdered(fromOrder: number) {
+    for (let i = fromOrder; i < this.promiseByOrder.length; i++) {
+      const currentPromise = this.promiseByOrder[i];
+      if (!currentPromise) return;
 
-      const isAlreadyFulfilled = eventPromise?.fulfilled;
-      if (isAlreadyFulfilled) return;
-
-      const prevEventPromise = this.promiseByOrder[i - 1];
-      const isInOrder = i === 0 || prevEventPromise?.fulfilled;
+      const prevPromise = this.promiseByOrder[i - 1];
+      const isInOrder = i === 0 || prevPromise?.fulfilled;
       if (!isInOrder) return;
 
-      eventPromise.resolve(i);
+      currentPromise.resolve(i);
     }
   }
 
-  ensureOrderFilled(order: number) {
+  private ensureOrderFilled(order: number) {
     const eventPromise = this.promiseByOrder[order];
     if (eventPromise) {
       throw new Error('Order already filled');
